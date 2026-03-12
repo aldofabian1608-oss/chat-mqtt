639b8f118553b3ce6a13a89797d036ddcb285625
aldofabian1608-oss <aldofabian1608@gmail.com>
2026-03-12 09:13:50 -0600

    Add files via upload

--- chat.py
+++ chat.py
@@ -1,27 +1,30 @@
 import paho.mqtt.client as mqtt
 import time
 
+# CONFIGURACIÓN DE CANALES SEPARADOS
 BROKER = "broker.hivemq.com"
 PORT = 1883
-TOPIC = "clase/redes/equipo/final" # DEBE SER IGUAL AL DEL HTML
+TOPIC_SUB = "clase/redes/equipo2/web"    # Escucha a la Web
+TOPIC_PUB = "clase/redes/equipo2/python" # Le responde a la Web
 
 def on_message(client, userdata, msg):
     print(f"\n[MENSAJE DE LA WEB]: {msg.payload.decode()}")
-    print("Tú: ", end="", flush=True)
+    print("Tú (Escribe mensaje): ", end="", flush=True)
 
 cliente = mqtt.Client()
 cliente.on_message = on_message
 
-print("Conectando al servidor de mensajes...")
+print("Conectando al Broker...")
 try:
     cliente.connect(BROKER, PORT, 60)
-    cliente.subscribe(TOPIC)
+    cliente.subscribe(TOPIC_SUB) # Solo se suscribe a lo que envía la web
     cliente.loop_start()
-    print("✅ LISTO. Escribe algo para mandarlo a GitHub Pages.")
+    print("✅ LISTO. Modo antiduplicados activo.")
     
     while True:
-        msj = input("Tú: ")
-        cliente.publish(TOPIC, msj)
+        msj = input("Tú (Escribe mensaje): ")
+        if msj.strip():
+            cliente.publish(TOPIC_PUB, msj) # Publica en el canal de respuesta
 except KeyboardInterrupt:
-    print("\nCerrando...")
+    print("\nDesconectando...")
     cliente.disconnect()
