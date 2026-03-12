cb80de38ac6ff1f7e56dcc09ccc19ccc4691fb80
aldofabian1608-oss <aldofabian1608@gmail.com>
2026-03-11 20:21:30 -0600

    Add files via upload

--- chat.py
+++ chat.py
@@ -2,28 +2,26 @@ import paho.mqtt.client as mqtt
 import time
 
 BROKER = "broker.hivemq.com"
-TOPIC = "clase/redes/equipo2" # MISMO TÓPICO QUE EL HTML
+PORT = 1883
+TOPIC = "clase/redes/equipo/final" # DEBE SER IGUAL AL DEL HTML
 
 def on_message(client, userdata, msg):
-    print(f"\n[MENSAJE RECIBIDO]: {msg.payload.decode()}")
+    print(f"\n[MENSAJE DE LA WEB]: {msg.payload.decode()}")
     print("Tú: ", end="", flush=True)
 
 cliente = mqtt.Client()
 cliente.on_message = on_message
 
-print("Conectando...")
-cliente.connect(BROKER, 1883)
-cliente.subscribe(TOPIC)
-cliente.loop_start()
-
-print("✅ Chat activo. Escribe algo o 'salir' para terminar.")
+print("Conectando al servidor de mensajes...")
 try:
+    cliente.connect(BROKER, PORT, 60)
+    cliente.subscribe(TOPIC)
+    cliente.loop_start()
+    print("✅ LISTO. Escribe algo para mandarlo a GitHub Pages.")
+    
     while True:
         msj = input("Tú: ")
-        if msj.lower() == 'salir': break
         cliente.publish(TOPIC, msj)
 except KeyboardInterrupt:
-    pass
-
-cliente.loop_stop()
-cliente.disconnect()
+    print("\nCerrando...")
+    cliente.disconnect()
