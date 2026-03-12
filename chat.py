2a84fdca7d4a3f0e148b9004b2fb86c6e19ee0af
aldofabian1608-oss <aldofabian1608@gmail.com>
2026-03-12 09:18:35 -0600

    Add files via upload

--- chat.py
+++ chat.py
@@ -1,30 +1,28 @@
 import paho.mqtt.client as mqtt
-import time
 
-# CONFIGURACIÓN DE CANALES SEPARADOS
 BROKER = "broker.hivemq.com"
 PORT = 1883
-TOPIC_SUB = "clase/redes/equipo2/web"    # Escucha a la Web
-TOPIC_PUB = "clase/redes/equipo2/python" # Le responde a la Web
+# TÓPICOS ESPEJO PARA LA WEB
+TOPIC_SUB = "clase/redes/equipo2/web_a_python"    # Recibe lo de la web
+TOPIC_PUB = "clase/redes/equipo2/python_a_web"    # Envía a la web
 
 def on_message(client, userdata, msg):
-    print(f"\n[MENSAJE DE LA WEB]: {msg.payload.decode()}")
-    print("Tú (Escribe mensaje): ", end="", flush=True)
+    print(f"\n[WEB]: {msg.payload.decode()}")
+    print("Tú: ", end="", flush=True)
 
 cliente = mqtt.Client()
 cliente.on_message = on_message
 
-print("Conectando al Broker...")
 try:
     cliente.connect(BROKER, PORT, 60)
-    cliente.subscribe(TOPIC_SUB) # Solo se suscribe a lo que envía la web
+    cliente.subscribe(TOPIC_SUB)
     cliente.loop_start()
-    print("✅ LISTO. Modo antiduplicados activo.")
+    print("✅ Conectado. Sistema antiduplicados listo.")
     
     while True:
-        msj = input("Tú (Escribe mensaje): ")
+        msj = input("Tú: ")
         if msj.strip():
-            cliente.publish(TOPIC_PUB, msj) # Publica en el canal de respuesta
+            cliente.publish(TOPIC_PUB, msj)
 except KeyboardInterrupt:
-    print("\nDesconectando...")
+    print("\nSaliendo...")
     cliente.disconnect()
